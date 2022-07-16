b02e91ea85cf75df034922c8219c49d52c42949b
VICTORIA GRAY <66696319+vgray2020@users.noreply.github.com>
2022-07-16 12:04:08 -0500

    Add files via upload

---
+++ Groovy Dog Java/src/main/java/com/javaform11/javaform/service/UsersService.java
@@ -0,0 +1,32 @@
+package com.javaform11.javaform.service;
+
+import com.javaform11.javaform.model.UsersModel;
+import com.javaform11.javaform.repository.UsersRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public UsersModel registerUser(String login, String password, String email){
+        if (login == null || password == null) {
+            return null;
+        } else {
+            UsersModel usersModel = new UsersModel();
+            usersModel.setLogin(login);
+            usersModel.setPassword(password);
+            usersModel.setEmail(email);
+            return usersRepository.save(usersModel);
+        }
+    }
+    public UsersModel authenticate(String login, String password){
+        return usersRepository.findByLoginAndPassword(login, password).orElse(null);
+    }
+
+
+}
---
+++ Groovy Dog Java/src/main/resources/static/script.js
@@ -0,0 +1,7 @@
+
+const openNav = () => {
+    document.getElementById('navbar').style.width = "200px";
+}
+const closeNav = () => {
+    document.getElementById('navbar').style.width = "0";
+}
